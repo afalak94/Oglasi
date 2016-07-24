0f5e818a22a3c22541818ccad6c0dd08345021ba
Lafrak <falak001@gmail.com>
2016-07-24 21:15:12 +0200

    Version 0.5

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -3,6 +3,6 @@ class Advertisement < ActiveRecord::Base
 	belongs_to :category
 	has_many :reviews
 
-	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
+	has_attached_file :image, styles: { large: "600x600>", medium: "400x200>", thumb: "150x150#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
